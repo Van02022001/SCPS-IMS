41ec728e8946afb59fb501727e07885d073b852e
trungnt <trung2kgl@gmail.com>
2023-09-15 05:16:00 +0700

    [Feature - Product] Add ProductRepository.java

---
+++ src/main/java/com/example/sparepartsinventorymanagement/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.example.sparepartsinventorymanagement.repository;
+
+import com.example.sparepartsinventorymanagement.entities.Category;
+import com.example.sparepartsinventorymanagement.entities.Product;
+import com.example.sparepartsinventorymanagement.entities.ProductStatus;
+import com.example.sparepartsinventorymanagement.entities.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByStatus(ProductStatus status);
+    List<Product> findByCategory(Category category);
+    List<Product> findByWarehouse(Warehouse warehouse);
+}

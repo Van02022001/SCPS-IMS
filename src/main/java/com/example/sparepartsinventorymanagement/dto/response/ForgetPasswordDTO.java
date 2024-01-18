8a395a64ee06b770633a8316ecf5d205d712b3eb
trungnt <trung2kgl@gmail.com>
2024-01-18 19:46:04 +0700

    [Feature] Update api forget password and reset password

--- src/main/java/com/example/sparepartsinventorymanagement/dto/response/ForgetPasswordDTO.java
+++ src/main/java/com/example/sparepartsinventorymanagement/dto/response/ForgetPasswordDTO.java
@@ -9,5 +9,4 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ForgetPasswordDTO {
     private String email;
-    private String message;
 }
--- src/main/java/com/example/sparepartsinventorymanagement/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/sparepartsinventorymanagement/service/impl/EmailServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.sparepartsinventorymanagement.service.impl;
 
 import com.example.sparepartsinventorymanagement.service.EmailService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -28,15 +31,17 @@ public class EmailServiceImpl implements EmailService {
         emailSender.send(message);
     }
 
-//    @Override
-//    public void sendPasswordResetEmail(String email, String token) {
-//        String resetUrl="http://localhost:8080/reset-password?token=" + token;
-//        String message ="Click the following link to reset your password: " + resetUrl;
-//
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setTo(email);
-//        mailMessage.setSubject("Password Reset Request");
-//        mailMessage.setText(message);
-//        emailSender.send(mailMessage);
-//    }
+    @Override
+    public void sendPasswordResetEmail(String email, String token) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String resetUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/api/v1/auth/reset-password/" + token;
+        String message ="Click the following link to reset your password: " + resetUrl;
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("qvanwork@outlook.com.vn");
+        mailMessage.setTo(email);
+        mailMessage.setSubject("Password Reset Request");
+        mailMessage.setText(message);
+        emailSender.send(mailMessage);
+    }
 }
